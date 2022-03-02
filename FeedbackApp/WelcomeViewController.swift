d9088cb07f63dee0144c2c92d1ea6287fe6f102a
algoives <jorge172@revature.net>
2022-03-02 18:15:53 -0500

    Add a first sample welcome page #12

---
+++ FeedbackApp/WelcomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WelcomeViewController.swift
+//  FeedbackApp
+//
+//  Created by Ives Murillo on 3/2/22.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var lwelcomeLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func servicesAction(_ sender: Any) {
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
