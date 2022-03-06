63d849458f14321831accef08a55dcd166e44d5a
algoives <jorge172@revature.net>
2022-03-06 01:11:31 -0500

    Set up Data core
    Set au datta core
    Add user entity to database

---
+++ User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  FeedbackApp
+//
+//  Created by Ives Murillo on 3/5/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
