a0902c345e161051a25f8408b770bf8943b5733f
algoives <jorge172@revature.net>
2022-03-06 18:45:42 -0500

    Add rating entity

--- FeedbackApp/views/RoomViewController.swift
+++ FeedbackApp/views/RoomViewController.swift
@@ -8,22 +8,68 @@
 import UIKit
 
 class RoomViewController: UIViewController {
-
+    
+    //IB outlets
+    
+    @IBOutlet weak var q1Good: UIButton!
+    
+    @IBOutlet weak var q1regular: UIButton!
+    
+    @IBOutlet weak var q1Bad: UIButton!
+    
+    //Rating properties
+    var rating = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//IB ACTIONS
+   
+    
+    //question 1
+    @IBAction func q1goodSelcted(_ sender: Any) {
+        rating = 5
+        
+        selectButton(q1Good,q1regular,q1Bad)
     }
-    */
-
+  
+    @IBAction func q1regularSelected(_ sender: Any) {
+        rating = 3
+        selectButton(q1regular, q1Bad, q1Good)
+    }
+    
+    @IBAction func q1BadSelected(_ sender: Any) {
+        rating = 2
+        selectButton(q1Bad, q1regular, q1Good)
+    }
+    
+    //Question2
+    
+    //Question3
+    
+    
+    //Submit Action
+    
+    @IBAction func submit(_ sender: Any) {
+        
+        DataBaseHelper.dataBaseHelper.addRoomRating(newRoomRating: rating)
+        
+    }
+    
+    //Privet functions
+    
+    //Fucntion to select a button
+    fileprivate func selectButton(_ selected : UIButton,_ unselected1 : UIButton,_ unselected2: UIButton
+    ) {
+        selected.backgroundColor = UIColor.darkGray
+        unselected1.backgroundColor = UIColor.lightGray
+        unselected2.backgroundColor = UIColor.lightGray
+    }
+    
+    
 }
---
+++ Rating+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Rating+CoreDataClass.swift
+//  FeedbackApp
+//
+//  Created by Ives Murillo on 3/6/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Rating)
+public class Rating: NSManagedObject {
+
+}
