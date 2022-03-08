3995dbf25baec4b9fbfa49fbdb8fe348defdeb83
Jorge Murillo <jorge172@revature.net>
2022-03-08 14:39:59 -0500

    Revert "Revert "Revert "Revert "Add logic for than you page""""

--- FeedbackApp/AppDelegate.swift
+++ FeedbackApp/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 
-@main
+ @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentCloudKitContainer(name: "FeedbackApp")
+        let container = NSPersistentCloudKitContainer(name: "DataBaseHelper")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
